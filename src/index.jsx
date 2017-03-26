0da3c559dd720a134242321a65b75427dbd0c2a7
ijmccallum <ijmccallum@hotmail.co.uk>
2017-03-26 16:01:08 -0400

    pulling in markup and styling for WL

--- src/index.jsx
+++ src/index.jsx
@@ -28,9 +28,64 @@ class Wikilogic extends React.Component {
 	render() {
 		return (
 			<Router history={history}>
-				<div className="jobs-app">
-					hello
-                </div>
+				<div>
+				<header className="header">
+					<div className="header__col">
+						<a href="/" className="site-flag">
+							<div href="/" className="site-flag__title">Wikilogic</div>
+							<div className="site-flag__tagline">tagline</div>
+						</a>
+					</div>
+					<div className="header__col">
+						<a href="/alchemy">Alchemy</a>
+						<a href="/d3">D3</a>
+						<a href="/d3v4">D3v4</a>
+					</div>
+					<div className="header__col header__col--search">
+						<div className="search">
+							<input type="text" className="search__input js-search-input" placeholder="Search claims"/>
+						</div>
+					</div>
+					<div className="header__col">
+						<div className="language-selector">
+							<a href="/lang" className="language-selector__current">En</a>
+
+							<div className="language-selector__dropdown">
+								<div className="language-selector__dropdown-option">Spanish</div>
+								<div className="language-selector__dropdown-option">French</div>
+								<div className="language-selector__dropdown-option">German</div>
+							</div>
+							
+						</div>
+					</div>
+                </header>
+				<main className="main">
+					<section className="section sidebar-layout">
+						<div className="sidebar-layout__main">
+							chart in here!
+						</div>
+						<div className="sidebar-layout__side">
+							<div className="search-results">
+								<div className="search-results__title"></div>
+								<div className="search-results__list">
+									<div className="js-search-results-list">search results here!</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</main>
+				<footer className="footer">
+					<div className="footer__col">
+						Wikilogic is maintainted by the <a href="www.wikilogicfoundation.org" target="_blank">Wikilogic Foundation</a>, a non-profit organisation...
+					</div>
+					<div className="footer__col">
+						Privacy policy
+					</div>
+					<div className="footer__col">
+						Terms of use
+					</div>
+				</footer>	
+				</div>
 			</Router>
 		);
 	}
