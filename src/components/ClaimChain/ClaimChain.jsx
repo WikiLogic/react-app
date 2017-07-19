8581467a3c4e4682d0cad929ef0019e242b1c366
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-18 20:57:38 -0400

    tidying claim chain

--- src/components/ClaimChain/ClaimChain.jsx
+++ src/components/ClaimChain/ClaimChain.jsx
@@ -8,27 +8,26 @@ import Notify from 'Services/notify';
  */
 
 export default class ClaimChain extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      chain: []
-    }
+      chain: [],
+    };
     this.premisClickHandler = this.premisClickHandler.bind(this);
     this.updatedClaimHandler = this.updatedClaimHandler.bind(this);
   }
 
   componentDidMount() {
-    //when this view opens, the ID of the claim is passed in by the props - ask the API for it!
-    api.getClaimDetailById(this.props.topClaimId)
+    // when this view opens, the ID of the claim is passed in by the props - ask the API for it!
+    API.getClaimDetailById(this.props.topClaimId)
       .then((data) => {
         this.setState({
           isLoading: false,
           chain: [{
             claim: data.claim,
-            highlighted_premis_id: ""
-          }]
+            highlighted_premis_id: '',
+          }],
         });
       }).catch((err) => {
         Notify.post(err);
@@ -37,34 +36,32 @@ export default class ClaimChain extends React.Component {
 
 
   premisClickHandler(premis, index) {
-    //When a chain link premis is clicked, we need to:
+    // When a chain link premis is clicked, we need to:
     let newChain = this.state.chain;
 
-    //clear out any lower level rows. 
+    // clear out any lower level rows. 
     newChain = newChain.slice(0, index + 1);
 
-    //highlight the premis that was clicked
+    // highlight the premis that was clicked
     newChain[index].highlighted_premis_id = premis.id;
 
-    //load in the arguments of that premis into the next level
-    api.getClaimDetailById(premis.id)
+    // load in the arguments of that premis into the next level
+    API.getClaimDetailById(premis.id)
       .then((data) => {
-
         newChain.push({
           claim: data.claim,
-          highlighted_premis_id: ""
+          highlighted_premis_id: '',
         });
 
         this.setState({ chain: newChain });
-
       }).catch((err) => {
         Notify.post(err);
       });
   }
 
   updatedClaimHandler(claim, index) {
-    //when a claim in the chain is updated (eg by adding a new argument) - this will replace that link in the chain
-    let newChain = this.state.chain;
+    // when a claim in the chain is updated (eg by adding a new argument) - this will replace that link in the chain
+    const newChain = this.state.chain;
     newChain[index].claim = claim;
 
     this.setState({ chain: newChain });
@@ -72,37 +69,40 @@ export default class ClaimChain extends React.Component {
 
   render() {
     let theChain = null;
-    let dougChain = null;
 
     if (this.state.isLoading) {
-
       return (
         <div>
           Loading...
-                </div>
+        </div>
       );
+    }
 
-    } else {
-
-      if (this.state.chain.length > 0) {
-        theChain = this.state.chain.map((chainLink, index) => {
-          if (chainLink.hasOwnProperty('claim')) {
-            return <ClaimDetail
+    if (this.state.chain.length > 0) {
+      theChain = this.state.chain.map((chainLink, index) => {
+        if (Object.prototype.hasOwnProperty.call(chainLink, 'claim')) {
+          return (
+            <ClaimDetail
               claim={chainLink.claim}
-              key={index}
+              key={chainLink.claim.id}
               highlightedPremisId={chainLink.highlighted_premis_id}
               premisClickHandler={(premis) => { this.premisClickHandler(premis, index); }}
               updatedClaimHandler={(claim) => { this.updatedClaimHandler(claim, index); }}
             />
-          }
-        });
-      }
-
-      return (
-        <div>
-          {theChain}
-        </div>
-      );
+          );
+        }
+        return null;
+      });
     }
+
+    return (
+      <div>
+        {theChain}
+      </div>
+    );
   }
 }
+
+ClaimDetail.propTypes = {
+  topClaimId: React.PropTypes.string.isRequired,
+};
