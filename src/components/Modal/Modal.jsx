641394f8ca87621c66100dbe4acf039466d7da2c
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-18 20:39:35 -0400

    tidy modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,29 +4,27 @@ import React from 'react';
  *
  */
 
-export default class Modal extends React.Component {
-  render() {
-    // Render nothing if the "show" prop is false
-    if (!this.props.show) {
-      return null;
-    }
+export default function Modal(props) {
+  // Render nothing if the "show" prop is false
+  if (!props.show) {
+    return null;
+  }
 
-    return (
-      <div className="modal">
-        <div className="modal__body">
-          <div className="modal__header">
-            <h3 className="modal__title">{this.props.title}</h3>
-            <button onClick={this.props.onClose}>
-              Close
-            </button>
-          </div>
-          <div className="modal__content">
-            {this.props.children}
-          </div>
+  return (
+    <div className="modal">
+      <div className="modal__body">
+        <div className="modal__header">
+          <h3 className="modal__title">{props.title}</h3>
+          <button onClick={props.onClose}>
+            Close
+          </button>
+        </div>
+        <div className="modal__content">
+          {props.children}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Modal.propTypes = {
