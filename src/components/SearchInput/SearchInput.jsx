b0f62bbef4501e0cc66f2dd152b4858c1e5a6869
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-18 20:37:14 -0400

    tidy search input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -7,39 +7,50 @@ import SearchIcon from 'Components/_Icons/SearchIcon';
  */
 
 export default class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = { value: '' };
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.inputValue !== '') {
+      this.setState({
+        value: nextProps.inputValue,
+      });
+    }
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.inputValue != '') {
-			this.setState({
-				value: nextProps.inputValue
-			});
-		}
-	}
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-	handleChange(event) {
-		this.setState({ value: event.target.value });
-	}
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.submissionHandler(this.state.value);
+  }
 
-	handleSubmit(event) {
-		event.preventDefault();
-		this.props.submissionHandler(this.state.value);
-	}
-
-	render() {
-		return (
-			<form className="search-form" onSubmit={this.handleSubmit}>
-				<input className="search-form__input" type="text" placeholder={this.props.placeholder} value={this.state.value} onChange={this.handleChange} />
-				<button className="search-form__submit" onClick={this.handleSubmit}>
-					<SearchIcon />
-				</button>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form className="search-form" onSubmit={this.handleSubmit}>
+        <input
+          className="search-form__input"
+          type="text"
+          placeholder={this.props.placeholder}
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button className="search-form__submit" onClick={this.handleSubmit}>
+          <SearchIcon />
+        </button>
+      </form>
+    );
+  }
 }
+
+SearchInput.propTypes = {
+  inputValue: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  submissionHandler: React.PropTypes.func.isRequired,
+  placeholder: React.PropTypes.string.isRequired,
+};
