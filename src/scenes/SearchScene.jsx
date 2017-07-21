d66bdc6e996b9486fcb006256076b2c0f2b75e72
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-20 21:56:10 -0400

    tidy search scene!

--- src/scenes/SearchScene.jsx
+++ src/scenes/SearchScene.jsx
@@ -9,82 +9,87 @@ import Notify from 'Services/notify';
  * The Search Results page
  */
 export default class SearchScene extends React.Component {
-	
-	constructor (props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			search_term: '',
-			search_results: []
-		};
+    this.state = {
+      search_term: '',
+      search_results: [],
+    };
 
-		this.searchClaims = this.searchClaims.bind(this);
-	}
+    this.searchClaims = this.searchClaims.bind(this);
+  }
 
-	componentDidMount(){
-		//get the search param to see if there is anything there
-		var URLsearchTerm = urlParameter.get('s', this.props.location.search);
+  componentDidMount() {
+    // get the search param to see if there is anything there
+    const URLsearchTerm = urlParameter.get('s', this.props.location.search);
 
-		//if there is
-		if (URLsearchTerm != '') {
-			//set the state so it renders
-			this.setState({
-				search_term: URLsearchTerm
-			});
-			
-			//and run the search
-			this.searchClaims(URLsearchTerm);
-		}
-	}
+    // if there is
+    if (URLsearchTerm !== '') {
+      // and run the search
+      this.searchClaims(URLsearchTerm);
+    }
+  }
 
-	searchClaims(search){
-		//run the search
-		if (isNaN(search)) {
-			console.log("searching by term", search);
-			API.searchClaimsByTerm(search)
-			.then((data) => {
-				this.setState({ search_results: data.claims });
-			}).catch((err) => {
-				Notify.post(err);
-			});
-		} else {
-			console.log("searching by id", search);
-			API.getClaimDetailById(search)
-			.then((data) => {
-				this.setState({ focused_claim: data.claim });
-			}).catch((err) => {
-				Notify.post(err);
-			});
-		}
+  searchClaims(search) {
+    // run the search
+    if (isNaN(search)) {
+      API.searchClaimsByTerm(search)
+        .then((data) => {
+          this.setState({ search_results: data.claims });
+        }).catch((err) => {
+          Notify.post(err);
+        });
+    } else {
+      API.getClaimDetailById(search)
+        .then((data) => {
+          this.setState({ focused_claim: data.claim });
+        }).catch((err) => {
+          Notify.post(err);
+        });
+    }
 
-		//add it to the url
-		this.props.history.push('/search?s=' + search);
+    // add it to the url
+    this.props.history.push(`/search?s=${search}`);
 
-		//set the state
-		this.setState({
-			search_term: search
-		});
-	}
+    // set the state
+    this.setState({
+      search_term: search,
+    });
+  }
 
 
-	render() {
-		return (
-			<div className="search-layout">
-				<div className="search-layout__header">
-					<div className="max-width-wrap">
+  render() {
+    return (
+      <div className="search-layout">
+        <div className="search-layout__header">
+          <div className="max-width-wrap">
 
-						<SearchInput submissionHandler={this.searchClaims} placeholder="Search Claims" inputValue={this.state.search_term}/>
+            <SearchInput
+              submissionHandler={this.searchClaims}
+              placeholder="Search Claims"
+              inputValue={this.state.search_term}
+            />
 
-					</div>
-				</div>
-				<div className="search-layout__results">
-					<div className="max-width-wrap">
+          </div>
+        </div>
+        <div className="search-layout__results">
+          <div className="max-width-wrap">
 
-						<SearchResults search_results={this.state.search_results}/>
+            <SearchResults search_results={this.state.search_results} />
 
-					</div>
-				</div>
-			</div>
-		);
-	}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+SearchScene.propTypes = {
+  history: React.PropTypes.shape({
+    push: React.PropTypes.func.isRequired,
+  }).isRequired,
+  location: React.PropTypes.shape({
+    search: React.PropTypes.string.isRequired,
+  }).isRequired,
+};
