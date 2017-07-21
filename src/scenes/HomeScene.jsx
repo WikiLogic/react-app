ee8813e1cba8be9b387437fc68234d0b1308377f
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-20 21:51:29 -0400

    tidying home scene!

--- src/scenes/HomeScene.jsx
+++ src/scenes/HomeScene.jsx
@@ -6,35 +6,39 @@ import SearchInput from 'Components/SearchInput/SearchInput.jsx';
  * @prop {*} name 
  */
 export default class SearchScene extends React.Component {
-	
-	constructor (props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			search_results: []
-		};
+    this.state = {
+      search_results: [],
+    };
 
-		this.searchClaims = this.searchClaims.bind(this);
-	}
+    this.searchClaims = this.searchClaims.bind(this);
+  }
 
-	searchClaims(search){
-        //go to /search?s=term
-        this.props.history.push('/search?s=' + search);
-	}
+  searchClaims(search) {
+    // go to /search?s=term
+    this.props.history.push(`/search?s=${search}`);
+  }
 
-	render() {
-		
-		return (
-			<div className="search-layout">
-				<div className="search-layout__header">
-					<div className="max-width-wrap">
-                        You've come home
+  render() {
+    return (
+      <div className="search-layout">
+        <div className="search-layout__header">
+          <div className="max-width-wrap">
+            You&#39;ve come home
 
-						<SearchInput submissionHandler={this.searchClaims} placeholder="Search Claims"/>
+            <SearchInput submissionHandler={this.searchClaims} placeholder="Search Claims" />
 
-					</div>
-				</div>
-			</div>
-		);
-	}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+SearchScene.propTypes = {
+  history: React.PropTypes.shape({
+    push: React.PropTypes.func.isRequired,
+  }).isRequired,
+};
