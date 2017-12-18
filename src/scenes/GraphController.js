43fde661e6ac0f70c27a61e67a6914f1b85a3c3a
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-12-18 07:37:51 -0500

    thougth process into writing

--- src/scenes/GraphController.js
+++ src/scenes/GraphController.js
@@ -1,6 +1,10 @@
 import { observable, action } from 'mobx';
 //main pulls in the scene and assigns this controller as it's state.
-
+// post about this. Nearly went down the route of splitting up the areas in a scene into 'views', but realised that way 
+// lies infinit ui taxonomies. better keep it simple. Scene and Component.
+// Mapped it out, then thought state is basically the same, map it out alongside
+// taken to it's conclusion, the scene needs a state - but as scenes are seperate from components, so shall their 
+// state be seperate from stores. Enter the controller! The state's version of a scene!
 export default class GraphController {
-    
+
 }
