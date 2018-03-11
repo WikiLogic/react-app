71a2b6e686ebee8f6ba6b88fdbc88536714cfb60
ijmccallum <ijmccallum@hotmail.co.uk>
2018-03-10 21:05:54 -0500

    prettier?

--- src/js/stores/graphClaimArgs.js
+++ src/js/stores/graphClaimArgs.js
@@ -1,13 +1,13 @@
-import { observable, action, computed } from 'mobx';
+import { observable, action, computed } from "mobx";
 
 /**
  * Manages the arguments for a claim in the graph
  */
 
 export default class GraphClaimArgs {
-  @observable premises = [];
+    @observable premises = [];
 
-  constructor(arg, graphConfig) {
-    this.premises = arg.premises;
-  }
+    constructor(arg, graphConfig) {
+        this.premises = arg.premises;
+    }
 }
